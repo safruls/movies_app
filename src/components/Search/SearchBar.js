935e7678eb1eed458268ab4bc88a9bc66ba6473c
safruls <safrul@xfers.com>
2021-10-13 18:07:55 +0700

    Create SearchBar & SearchResult components

---
+++ src/components/Search/SearchBar.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Input } from "antd";
+import styled from "@emotion/styled";
+import { fetchSearchedMovies } from "store/searchMovies/action";
+
+const SearchBar = ({ page }) => {
+  const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    if (page > currentPage) {
+      dispatch(fetchSearchedMovies(searchParams, page));
+      setCurrentPage(page);
+    }
+  }, [page]);
+
+  const onSearch = (value) => {
+    dispatch(fetchSearchedMovies(value, 1));
+    setSearchParams(value);
+    setCurrentPage(1);
+  };
+
+  return (
+    <SearchSection>
+      <Input.Search
+        placeholder="Search movies title..."
+        allowClear
+        enterButton="Search"
+        size="large"
+        onSearch={onSearch}
+      />
+    </SearchSection>
+  );
+};
+
+export default SearchBar;
+
+const SearchSection = styled.div`
+  width: 100%;
+  flex-basis: 50%;
+  flex: 1;
+`;
---
+++ src/components/Search/SearchResult.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Card } from "antd";
+import styled from "@emotion/styled";
+import ModalComponent from "components/Modal";
+
+const SearchResult = ({ movie }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const { Meta } = Card;
+
+  return (
+    <>
+      <CardContainer key={movie?.Title}>
+        <Card
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <StyledImage
+              alt={movie?.Title}
+              src={movie?.Poster}
+              onClick={() => {
+                setOpenModal(true);
+              }}
+            />
+          }
+        >
+          <Meta
+            title={
+              <>
+                <Link to={`/${movie?.Title}`}>
+                  <StyledParagraph>{movie?.Title}</StyledParagraph>
+                </Link>
+              </>
+            }
+            description={
+              <Description>
+                <StlyedUndorderedList>
+                  <StyledItem>
+                    Released Year: <strong>{movie?.Year}</strong>
+                  </StyledItem>
+                  <StyledItem>
+                    IMDB ID: <strong>{movie?.imdbID}</strong>
+                  </StyledItem>
+                  <StyledItem>
+                    Type: <strong>{movie?.Type}</strong>
+                  </StyledItem>
+                </StlyedUndorderedList>
+              </Description>
+            }
+          />
+        </Card>
+      </CardContainer>
+      {
+        <ModalComponent
+          movie={movie}
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+        />
+      }
+    </>
+  );
+};
+
+export default SearchResult;
+
+const CardContainer = styled.div`
+  margin: 10px 10px;
+
+  & .ant-card-meta-title {
+    overflow: visible;
+    white-space: normal;
+    word-break: break-all;
+  }
+`;
+
+const StyledImage = styled.img`
+  height: 320px;
+`;
+
+const Description = styled.div``;
+
+const StlyedUndorderedList = styled.ul`
+  display: inline-block;
+  margin-block-start: 1px;
+  margin-block-end: 1px;
+  padding-inline-start: 18px;
+`;
+
+const StyledItem = styled.li`
+  color: black;
+`;
+
+const StyledParagraph = styled.p`
+  word-break: break-all;
+`;
